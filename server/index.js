ac8ff6ed83d274eef3815fd741e4326baf8da315
taki <taki@gmail.com>
2024-10-18 19:01:04 +0100

    Fix port binding for Render

--- server/index.js
+++ server/index.js
@@ -39,5 +39,5 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
-  console.log("SERVER RUNNING");
+  console.log(`Server running on port ${PORT}`);
 });
